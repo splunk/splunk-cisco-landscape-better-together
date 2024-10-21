aed5f507f1af0be9fb535e4728e455b079a0bd4a
Isaac Fonseca <ifonsecamonge@splunk.com>
2024-10-21 15:46:39 -0600

    sankey chart - custom changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import CardLayout from './components/CardView/CardLayout';
 import SankeyChart from './components/SankeyChart/SankeyChart';
+import { Button } from '@mui/material';
 
 function App() {
+
+  const data = ['Collaboration', 'Security', 'Networking', 'Application Performance']
+  const [type, setType] = useState('Collaboration')
+  const [index, setIndex] = useState(0)
+
   return (
-    <div className="App">
-      <CardLayout />
-      <SankeyChart type={'Collaboration'} />
-    </div>
+    <>
+      <Button type='button' variant='contained' style={{ margin: '1rem' }} onClick={() => {
+        let value = 0
+        if (index + 1 !== 4) value = index + 1
+        setType(data[value])
+        setIndex(value)
+      }}>Next {index + 1}</Button>
+      <div className="App">
+        <SankeyChart type={type} />
+      </div>
+    </>
   );
 }
 

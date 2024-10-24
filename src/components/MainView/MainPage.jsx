71f97fa3fca714232472c4229abb03699dfa9f63
ahoang-splunk <ahoang@splunk.com>
2024-10-24 00:00:32 +0000

    Added logo to bottom of page

--- src/components/MainView/MainPage.jsx
+++ src/components/MainView/MainPage.jsx
@@ -14,6 +14,7 @@ import SettingsIcon from '@mui/icons-material/Settings';
 import Button from '@mui/material/Button';
 import SankeyChart from '../SankeyChart/SankeyChart';
 import CardLayout from '../CardView/CardLayout';
+import ciscoLogo from '../../ciscoLogo.png';
 
 const tabStyles = {
   color: 'white',
@@ -75,37 +76,47 @@ export default function VerticalTabs() {
 
   return (
     <Box
-      sx={{ position: 'relative', backgroundImage: 'linear-gradient(to bottom right, #039EC8, #071E34 70%, #C27221, #C42C4F)', display: 'flex', flexDirection: 'row', minHeight: '100vh', alignItems: 'flex-start' }}
+      sx={{ position: 'relative', backgroundImage: 'linear-gradient(to bottom right, #039EC8, #071E34 70%, #C27221, #C42C4F)', display: 'flex', minHeight: '100vh', alignItems: 'flex-start', flexDirection: 'column' }}
     >
-      <Box sx={{ flexBasis: '20%'}}>
-        {/* Tabs */}
-        <Tabs
-          orientation="vertical"
-          value={value}
-          onChange={handleChange}
-          sx={{ borderRight: 1, borderColor: 'divider', height: 'auto', width: '300px' }}
-        >
-          <Tab label="Networking" sx={{ color: 'white', paddingTop: '120px', fontSize: '20px', '&.Mui-selected': {color: '#0f0f0f'} }} icon={<HubIcon sx={{ fontSize: '2.5rem' }} />} {...a11yProps(0)} />
-          <Tab label="Security" sx={{ color: 'white', paddingTop: '120px', fontSize: '20px', '&.Mui-selected': {color: '#0f0f0f'} }} icon={<SecurityIcon sx={{ fontSize: '2.5rem' }} />} {...a11yProps(1)} />
-          <Tab label="Collaboration" sx={{ color: 'white', paddingTop: '120px', fontSize: '20px', '&.Mui-selected': {color: '#0f0f0f'} }} icon={<GroupsIcon sx={{ fontSize: '3rem' }} />} {...a11yProps(2)} />
-          <Tab label="Application Performance" sx={{ color: 'white', paddingTop: '120px', fontSize: '20px', '&.Mui-selected': {color: '#0f0f0f'} }} icon={<SpeedIcon sx={{ fontSize: '3rem' }} />} {...a11yProps(3)} />
-        </Tabs>
+      <Box 
+        sx={{
+          display: 'flex', 
+          flexGrow: 1, 
+        }}
+      >
+        <Box sx={{ flexBasis: '20%'}}>
+          {/* Tabs */}
+          <Tabs
+            orientation="vertical"
+            value={value}
+            onChange={handleChange}
+            sx={{ borderRight: 1, borderColor: 'divider', height: 'auto', width: '300px' }}
+          >
+            <Tab label="Networking" sx={{ color: 'white', paddingTop: '120px', fontSize: '20px', '&.Mui-selected': {color: '#0f0f0f'} }} icon={<HubIcon sx={{ fontSize: '2.5rem' }} />} {...a11yProps(0)} />
+            <Tab label="Security" sx={{ color: 'white', paddingTop: '120px', fontSize: '20px', '&.Mui-selected': {color: '#0f0f0f'} }} icon={<SecurityIcon sx={{ fontSize: '2.5rem' }} />} {...a11yProps(1)} />
+            <Tab label="Collaboration" sx={{ color: 'white', paddingTop: '120px', fontSize: '20px', '&.Mui-selected': {color: '#0f0f0f'} }} icon={<GroupsIcon sx={{ fontSize: '3rem' }} />} {...a11yProps(2)} />
+            <Tab label="Application Performance" sx={{ color: 'white', paddingTop: '120px', fontSize: '20px', '&.Mui-selected': {color: '#0f0f0f'} }} icon={<SpeedIcon sx={{ fontSize: '3rem' }} />} {...a11yProps(3)} />
+          </Tabs>
+        </Box>
+        <Box sx={{ flexBasis: '80%', paddingTop: '100px' }}>
+          {/* Tab content */}
+          <TabPanel value={value} index={0}>
+            { selectedView === 'Show cards layout' ? (<CardLayout category="Networking"/>) : (<SankeyChart category={'Networking'} />) }
+          </TabPanel>
+          <TabPanel value={value} index={1}>
+            { selectedView === 'Show cards layout' ? (<CardLayout category="Security"/>) : (<SankeyChart category={'Security'} />) }
+          </TabPanel>
+          <TabPanel value={value} index={2}>
+            { selectedView === 'Show cards layout' ? (<CardLayout category="Collaboration"/>) : (<SankeyChart category={'Collaboration'} />) }
+          </TabPanel>
+          <TabPanel value={value} index={3}>
+            { selectedView === 'Show cards layout' ? (<CardLayout category="Application Performance"/>) : (<SankeyChart category={'Application Performance'} />) }
+          </TabPanel>
+        </Box>
       </Box>
-      <Box sx={{ flexBasis: '80%', paddingTop: '100px'}}>
-        {/* Tab content */}
-        <TabPanel value={value} index={0}>
-          { selectedView === 'Show cards layout' ? (<CardLayout category="Networking"/>) : (<SankeyChart category={'Networking'} />) }
-        </TabPanel>
-        <TabPanel value={value} index={1}>
-          { selectedView === 'Show cards layout' ? (<CardLayout category="Security"/>) : (<SankeyChart category={'Security'} />) }
-        </TabPanel>
-        <TabPanel value={value} index={2}>
-          { selectedView === 'Show cards layout' ? (<CardLayout category="Collaboration"/>) : (<SankeyChart category={'Collaboration'} />) }
-        </TabPanel>
-        <TabPanel value={value} index={3}>
-          { selectedView === 'Show cards layout' ? (<CardLayout category="Application Performance"/>) : (<SankeyChart category={'Application Performance'} />) }
-        </TabPanel>
-      </Box>
+      {/* <Box sx={{ position:"relative", bottom:'10px', width:"100%" }}>
+        <img src={ciscoLogo} alt="Logo" style={{ width: '200px', height: 'auto' }}/>  
+      </Box>  */}
       {/* Setting and info pop up */}
       <Box sx={{ position: 'absolute', bottom: 100, right: 100 }}>
         <Button onClick={handleClickOpen} sx={{ color: 'white' }}>
